dc4c146e17704ba40f50970ff41b48f519ea6544
Vitalii Iakushev <vsiakushev@gmail.com>
2023-01-26 22:00:43 +0400

    Initial Commit

---
+++ CoreData-StockList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreData-StockList
+//
+//  Created by Vitaliy Iakushev on 26.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoreData-StockList/CoreData_StockListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreData_StockListApp.swift
+//  CoreData-StockList
+//
+//  Created by Vitaliy Iakushev on 26.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoreData_StockListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
